bb312d4ad505691c5fe1eedef5b46c8a857c83b7
Matt Jones <bigboxer23@gmail.com>
2022-12-19 14:31:49 -0600

    Update to use utility package for index persistence.

--- src/main/java/com/bigboxer23/meural_control/SchedulerComponent.java
+++ src/main/java/com/bigboxer23/meural_control/SchedulerComponent.java
@@ -2,7 +2,7 @@ package com.bigboxer23.meural_control;
 
 import com.bigboxer23.meural_control.data.*;
 import com.bigboxer23.meural_control.google.GooglePhotosComponent;
-import com.bigboxer23.meural_control.util.FilePersistentIndex;
+import com.bigboxer23.utils.FilePersistentIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
