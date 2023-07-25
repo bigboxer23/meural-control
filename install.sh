d07fc522dfc402849e61d57e5f23548074362a1e
Matt Jones <bigboxer23@gmail.com>
2023-07-25 13:56:58 -0500

    add service installation

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com"
 ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23"
 ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/meural-control"
 ssh -t pi@$host -o StrictHostKeyChecking=no "mkdir /home/pi/com/bigboxer23/meural-control/1.0"
-
+scp -o StrictHostKeyChecking=no -r scripts/meural.service pi@$host:/lib/systemd/system
+ssh -t pi@$host -o StrictHostKeyChecking=no "sudo systemctl daemon-reload"
+ssh -t pi@$host -o StrictHostKeyChecking=no "sudo systemctl enable meural.service"
 #This can transfer your google auth tokens directory to a remote server which can't directly run the OAuth flow
 #scp -o StrictHostKeyChecking=no -r tokens pi@$host:/home/pi/com/bigboxer23/meural-control/1.0
