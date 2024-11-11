8cef2785bf46daec74f50b76d30041ac82a6a5c0
Matt Jones <bigboxer23@gmail.com>
2024-11-11 08:37:39 -0600

    fix: reset api token on error so we fetch token again (#264)

--- src/test/java/com/bigboxer23/meural_control/OpenAIComponentTest.java
+++ src/test/java/com/bigboxer23/meural_control/OpenAIComponentTest.java
@@ -2,21 +2,11 @@ package com.bigboxer23.meural_control;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.bigboxer23.meural_control.data.OpenAIImageGenerationBody;
-import com.bigboxer23.meural_control.data.OpenAIImageGenerationResponse;
 import com.bigboxer23.meural_control.data.SourceItem;
-import com.bigboxer23.utils.http.OkHttpUtil;
-import com.squareup.moshi.Moshi;
-import java.io.IOException;
 import java.util.Optional;
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
